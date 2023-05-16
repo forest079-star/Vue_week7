9cdf4c65683a80952c097f752686d86691fa96cb
forest079 <bluegx08@gmail.com>
2023-05-16 15:54:28 +0800

    globalProperties/provide  的差別

--- src/main.js
+++ src/main.js
@@ -12,14 +12,19 @@ import App from './App.vue'
 import router from './router'
 
 // custom plugin
-import { date } from './methods/filters'
+import { date, unixTime } from './methods/filters'
 
 const pinia = createPinia()
 
 const app = createApp(App)
 
+// app.config.globalProperties.$filters = {
+//   date,
+//   currency,
+// };
+
 app
-  .provide('$filters', { date })
+  .provide('$filters', { date, unixTime })
   .use(pinia)
   .use(router)
   .use(CKEditor)
@@ -27,3 +32,5 @@ app
   .use(VueAxios, axios)
 
 app.mount('#app')
+
+// 总的来说，如果你想在组件树中传递数据或方法，provide / inject 是一个好选择。如果你想在所有组件中都能访问某个属性或方法，可以使用 app.config.globalProperties。请根据你的具体需求选择合适的方法。
